6cffd67535ced762738ffed310b63f588b0c1e54
이찬혁 <chlee01@crscube.io>
2024-12-03 10:37:16 +0900

    .sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,29 @@
-# !/bin/bash
-# 타겟 프로젝트에서 bash 실행
+#!/bin/bash
+#./deploy.sh C:\Users\{USER NAME}\..\{TARGET_PROJECT}
+set -e
+
+RED='\033[0;31m'
+NC='\033[0m'
 
 REPO_PATH=${PWD}
-REPO_NAME=${REPO_PATH##*/}
+TARG_PATH=${0}
 
-echo 'target path : '$REPO_PATH
-echo 'target name : '$REPO_NAME
+echo 'repository path : '$REPO_PATH
+echo 'target path : '$TARG_PATH
 
-mvn -Dmaven.test.skip=true -DaltDeploymentRepository=snapshot-repo::default::file://${REPO_PATH}/snapshots clean deploy
-cd ${local_occidere_maven_repo}
+if [ "$#" -eq 0 ]; then
+  echo -e "${RED}Error: No arguments provided.${NC}"
+  exit 1
+fi
+if [ ! -f "$TARG_PATH/pom.xml" ]; then
+    echo -e "${RED}Error: pom.xml not found in ${TARG_PATH}${NC}"
+    exit 1
+fi
+
+mvn -Dmaven.test.skip=true -DaltDeploymentRepository=snapshot-repo::default::file://$REPO_PATH/snapshots clean deploy
+cd $TARG_PATH
 
 git add .
 git status
-git commit -m "release new version of ${PROJ_NAME}"
+git commit -m "release new version of ${TARG_PATH##*/}"
 git push origin release
