2eff5ef4989cc4503847ad68f68bfc07d6b9359d
root <root@chlee-144.>
2024-12-04 11:16:20 +0900

    release new version of cube-pdf

--- deploy.sh
+++ deploy.sh
@@ -29,7 +29,6 @@ chmod 777 $REPO_PATH/releases
 chmod 777 $REPO_PATH/snapshots
 cd $TARG_PATH
 #mvn clean deploy -Dmaven.test.skip=true -DaltDeploymentRepository=release::default::file://$REPO_PATH/releases
-mvn jar:jar
 mvn -Dmaven.test.skip=true -DaltDeploymentRepository=snapshot-repo::default::file://$REPO_PATH/snapshots clean deploy
 cd $REPO_PATH
 
